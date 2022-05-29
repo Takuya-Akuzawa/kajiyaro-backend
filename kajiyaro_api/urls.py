40c06d6ab6fdd3def8cca88896156620509c83ce
Takuya Akuzawa <kuat2245@gmail.com>
2022-05-29 23:03:12 +0900

    ListViiew, RetrieveViewを定義。Readは未認証でもアクセス可能へ変更。

--- kajiyaro_api/urls.py
+++ kajiyaro_api/urls.py
@@ -2,13 +2,20 @@ from django.conf.urls import include
 from django.db import router
 from django.urls import path
 from rest_framework import routers
-from .views import CategoryViewSet, HouseworkViewSet
+from .views import HouseworkViewSet, HouseworkListView, HouseworkRetrieveView, \
+                    CategoryViewSet, CategoryListView, CategoryRetrieveView
 
 router = routers.DefaultRouter()
 router.register(r'categories', CategoryViewSet, basename='categories')
 router.register(r'houseworks', HouseworkViewSet, basename='houseworks')
 
 urlpatterns = [
+    path('list-housework/', HouseworkListView.as_view(), name='list-housework'),
+    path('detail-housework/<str:pk>/', HouseworkRetrieveView.as_view(), name='detail-housework'),
+    
+    path('list-category/', CategoryListView.as_view(), name='list-category'),
+    path('detail-category/<str:pk>/', CategoryRetrieveView.as_view(), name='detail-category'),
+    
     path('auth/', include('djoser.urls.jwt')),
     path('', include(router.urls)),
 ]
--- kajiyaro_api/views.py
+++ kajiyaro_api/views.py
@@ -1,16 +1,38 @@
 from rest_framework.permissions import AllowAny
-from rest_framework import viewsets
+from rest_framework import viewsets, generics
 from .serializers import CategorySerializer, HouseworkSerializer
 from .models import Category, Housework
 
+class HouseworkListView(generics.ListAPIView):
+    queryset = Housework.objects.all()
+    serializer_class = HouseworkSerializer
+    permission_classes = (AllowAny,)
 
-class CategoryViewSet(viewsets.ModelViewSet):
-    queryset = Category.objects.all()
-    serializer_class = CategorySerializer
-    # permission_classes = (AllowAny,)
+
+class HouseworkRetrieveView(generics.RetrieveAPIView):
+    queryset = Housework.objects.all()
+    serializer_class = HouseworkSerializer
+    permission_classes = (AllowAny,)
 
 
 class HouseworkViewSet(viewsets.ModelViewSet):
     queryset = Housework.objects.all()
     serializer_class = HouseworkSerializer
+    # permission_classes = (AllowAny,)
+
+class CategoryListView(generics.ListAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = (AllowAny,)
+
+
+class CategoryRetrieveView(generics.RetrieveAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = (AllowAny,)
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
     # permission_classes = (AllowAny,)
